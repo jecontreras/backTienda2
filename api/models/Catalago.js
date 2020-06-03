56175010e143c4ece854ac73d881c714d1039d5b
jecontreras <joseeduar147@gmail.com>
2020-06-03 18:20:04 -0500

    cambios nuevos

---
+++ api/models/Catalago.js
@@ -0,0 +1,21 @@
+/**
+ * Catalago.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    titulo:{
+      type: 'string'
+    },
+    estado:{
+      type: 'integer' // 0 activo, 2 inactivo
+    }
+  },
+
+};
+
---
+++ api/models/CatalagoDetallado.js
@@ -0,0 +1,25 @@
+/**
+ * CatalagoDetallado.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    catalago:{
+      model: 'catalago'
+    },
+    producto:{
+      model: 'tblproductos'
+    },
+    estado:{
+      type: 'integer' // 0 activo, 2 inactivo
+    }
+
+  },
+
+};
+
