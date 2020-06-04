b7037f3c14beea21e8e4f59eeeac2306586b99c2
jecontreras <joseeduar147@gmail.com>
2020-06-04 15:31:39 -0500

    cambios nuevos

--- api/controllers/CatalagoDetalladoController.js
+++ api/controllers/CatalagoDetalladoController.js
@@ -6,14 +6,32 @@
  */
 
 let Procedures = Object();
+var base64Img = require('base64-img');
+
 Procedures.querys = async (req, res)=>{
 	let params = req.allParams();
     let resultado = Object();
     // console.log("***", params);
 	resultado = await QuerysServices(CatalagoDetallado, params);
 	for( let row of resultado.data ){
-		if( row.producto ) row.producto = await Tblproductos.findOne({ id: row.producto });
+		if( row.producto ) {
+			let producto = await Tblproductos.findOne({ id: row.producto });
+			row.producto = producto;
+			if( row.producto ) { 
+				base = await Procedures.FormatoBase64( row.producto.foto );
+				row.base64 = base;
+			}
+		}
 	}
 	return res.ok(resultado);
+}
+
+Procedures.FormatoBase64 = ( foto )=>{
+	return new Promise( resolve=>{
+		base64Img.requestBase64(foto, function(err, res, body) {
+			// console.log("********", res, body);
+			resolve( body );
+		});
+	});
 }
 module.exports = Procedures;
